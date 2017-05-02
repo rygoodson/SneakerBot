a8e849b6e685177074546791c91b0bea4be98a2f
Cody <Cody@CodyPC>
2017-05-02 16:04:26 -0700

    Maven

--- src/main/java/sneakerbot/Bot.java
+++ src/main/java/sneakerbot/Bot.java
@@ -1,8 +1,19 @@
 package main.java.sneakerbot;
 
+import main.java.sneakerbot.thread.ThreadPool;
+
 public class Bot {
+	
+	static ThreadPool pool;
 
 	public static void main(String[] args) {
-		int test = 0;
+		int count = 1; // proxy count (default=1)
+		
+		pool = new ThreadPool(count);
+	}
+	
+	static {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
 	}
 }
---
+++ src/main/java/sneakerbot/atc/Adidas.java
@@ -0,0 +1,15 @@
+package main.java.sneakerbot.atc;
+
+public class Adidas implements Runnable {
+	
+	Adidas(String proxy) {
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
