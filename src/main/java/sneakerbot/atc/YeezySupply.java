74a5f94c75a37c54101e91a42e7174c8c45ae630
Cody <Cody@CodyPC>
2017-05-05 03:08:45 -0700

    Added ys

---
+++ src/main/java/sneakerbot/atc/YeezySupply.java
@@ -0,0 +1,15 @@
+package main.java.sneakerbot.atc;
+
+public class YeezySupply implements Runnable {
+	
+	YeezySupply(String proxy) {
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
